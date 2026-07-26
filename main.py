3b26cc91235b5a1eb6f98e2b4569c2222ab3bd07
admin <sokolov.vladislav.98@inbox.ru>
2026-07-26 11:37:08 +0300

    dev

--- main.py
+++ main.py
@@ -1,7 +1,91 @@
 import json
+import requests
 
-from tradingview import get_ideas
 
+def get_session():
+    session = requests.Session()
+
+    session.headers.update({
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/116.0.0.0 Safari/537.36"
+        ),
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+    })
+
+    session.cookies.set(
+        "cookiePrivacyPreferenceBannerProduction",
+        "notApplicable",
+        domain=".tradingview.com",
+    )
+
+    session.cookies.set(
+        "cookiesSettings",
+        '{"analytics":true,"advertising":true}',
+        domain=".tradingview.com",
+    )
+
+    session.cookies.set(
+        "device_t",
+        "ZDJIWEJnOjA.FzlbqZZZDUn3rAZsUnCck0IeeBQdNHWNGufTK9Sfq0g",
+        domain=".tradingview.com",
+    )
+
+    session.cookies.set(
+        "sessionid",
+        "...",
+        domain=".tradingview.com",
+    )
+
+    session.cookies.set(
+        "sessionid_sign",
+        "...",
+        domain=".tradingview.com",
+    )
+
+    session.cookies.set(
+        "tv_ecuid",
+        "...",
+        domain=".tradingview.com",
+    )
+
+    return session
+
+def get_ideas(ticker: str, limit: int = 5):
+
+    session = get_session()
+
+    url = (
+        f"https://ru.tradingview.com/"
+        f"symbols/{ticker}/ideas/?component-data-only=1"
+    )
+
+    headers = {
+        "referer": (
+            f"https://ru.tradingview.com/"
+            f"symbols/{ticker}/ideas/?sort=recent"
+        ),
+        "x-requested-with": "XMLHttpRequest",
+        "x-language": "ru",
+    }
+
+    response = session.get(
+        url,
+        headers=headers,
+        timeout=20,
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    return (
+        data.get("data", {})
+            .get("ideas", {})
+            .get("data", {})
+            .get("items", [])[:limit]
+    )
 
 def main(context):
 
